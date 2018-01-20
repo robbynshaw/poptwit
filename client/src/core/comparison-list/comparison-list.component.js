e9d82201074a0dcd4f7cb017a4a6defcc1dfa709
Robby Shaw <robbynshaw@gmail.com>
2018-01-20 02:23:38 -0600

    Wrapped component to keep global scope clean

--- client/src/core/comparison-list/comparison-list.component.js
+++ client/src/core/comparison-list/comparison-list.component.js
@@ -1,18 +1,20 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular
-    .module('popTwit.comparisonList')
-    .component('comparisonList', {
-        templateUrl: 'src/core/comparison-list/comparison-list.template.html',
-        controller: ComparisonListController
-    });
+    angular
+        .module('popTwit.comparisonList')
+        .component('comparisonList', {
+            templateUrl: 'src/core/comparison-list/comparison-list.template.html',
+            controller: ComparisonListController
+        });
 
-function ComparisonListController() {
-    this.comparisons = [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-        { id: 4 },
-        { id: 5 },
-    ];
-}
+    function ComparisonListController() {
+        this.comparisons = [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 },
+            { id: 4 },
+            { id: 5 },
+        ];
+    }
+})();
